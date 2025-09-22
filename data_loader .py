c2c845d423a54bca2e62d40411f9c4dadb744a24
alimostafa19967777-ux <alimostafa19967777@gmail.com>
2025-09-22 22:18:50 +0700

    Delete data_loader .py

--- data_loader .py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-FILE_ID = "1Ni4rvDTwdX5LpSsI8uur47ISeQ4fkRmW"  # ID файла на Google Drive
-file_url = f"https://drive.google.com/uc?id={FILE_ID}"
-
-raw_data = pd.read_csv(file_url)     # читаем файл
-
-raw_data.head(10) 
